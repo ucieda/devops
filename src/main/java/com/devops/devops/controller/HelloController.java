37bfa41e33f820536802b55401b781be7d4fa07d
Wilder Ucieda <winner10124@hotmail.com>
2018-08-03 22:50:40 -0400

    hola mundo

---
+++ src/main/java/com/devops/devops/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.devops.devops.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @RequestMapping("/")
+    String hellow()
+    {
+        System.out.println("<<< Mundo >>>>");
+        System.out.println("<<< Hola >>>>");
+        return "Hello World!";
+
+    }
+}
